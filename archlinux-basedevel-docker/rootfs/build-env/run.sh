1bf76380daefbe69bf7d00b1a7cfe91e8c56d2aa
Martina Pietruschka <post@martinapietruschka.de>
2020-03-11 23:23:45 +0100

    handle return code Xth try

--- archlinux-basedevel-docker/rootfs/build-env/run.sh
+++ archlinux-basedevel-docker/rootfs/build-env/run.sh
@@ -14,9 +14,8 @@ chown -R build:build /build
 cd /build
 
 # Don't fail the rest of this script
-su - build -s /bin/sh -c "$( IFS=$' '; echo "$@" )" || true
-EXITCODE=$?
-
+EXITCODE=0
+su - build -s /bin/sh -c "$( IFS=$' '; echo "$@" )" || EXITCODE=$?
 
 chown -R --reference=/src/. /build-target/*
 cp -Rf --preserve=mode,ownership,timestamps /build-target/* /src/
