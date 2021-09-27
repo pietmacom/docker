e3bd1a6629acd86fe4f6f9d03915c83d818add23
Marti McFly <447353+mpietruschka@users.noreply.github.com>
2021-09-27 20:38:42 +0200

    Update setup.sh
    add discard

--- archlinux-jenkins-docker/slave/setup.sh
+++ archlinux-jenkins-docker/slave/setup.sh
@@ -138,16 +138,16 @@ mkdir -p /media/base-overlay
 mkdir -p /media/data
 
 ## crypttab
-echo "swap $(find -L /dev/disk/by-id -samefile ${WORKDRIVE}2 | head -n 1) /dev/urandom swap,cipher=aes-cbc-essiv:sha256,size=256,no-read-workqueue,no-write-workqueue" >> /etc/crypttab
-echo "tmp $(find -L /dev/disk/by-id -samefile ${WORKDRIVE}3 | head -n 1) /dev/urandom tmp,cipher=aes-cbc-essiv:sha256,size=256,no-read-workqueue,no-write-workqueue" >> /etc/crypttab
-echo "base-overlay $(find -L /dev/disk/by-id -samefile ${WORKDRIVE}4 | head -n 1) /dev/urandom tmp,cipher=aes-cbc-essiv:sha256,size=256,no-read-workqueue,no-write-workqueue" >> /etc/crypttab
+echo "swap $(find -L /dev/disk/by-id -samefile ${WORKDRIVE}2 | head -n 1) /dev/urandom swap,discard,cipher=aes-cbc-essiv:sha256,size=256,no-read-workqueue,no-write-workqueue" >> /etc/crypttab
+echo "tmp $(find -L /dev/disk/by-id -samefile ${WORKDRIVE}3 | head -n 1) /dev/urandom tmp,discard,cipher=aes-cbc-essiv:sha256,size=256,no-read-workqueue,no-write-workqueue" >> /etc/crypttab
+echo "base-overlay $(find -L /dev/disk/by-id -samefile ${WORKDRIVE}4 | head -n 1) /dev/urandom tmp,discard,cipher=aes-cbc-essiv:sha256,size=256,no-read-workqueue,no-write-workqueue" >> /etc/crypttab
 
 
 ## fstab
 echo "$(find -L /dev/disk/by-id -samefile ${WORKDRIVE}1 | head -n 1) /media/base ext4 ro 0 2" >> /etc/fstab
 echo "/dev/mapper/swap none swap defaults 0 0" >> /etc/fstab
-echo "/dev/mapper/tmp /tmp ext4 rw,noexec,nosuid,nodev 0 0" >> /etc/fstab
-echo "/dev/mapper/base-overlay /media/base-overlay ext4 defaults,noatime,nodiratime 0 0" >> /etc/fstab
+echo "/dev/mapper/tmp /tmp ext4 rw,noexec,nosuid,nodev,discard 0 0" >> /etc/fstab
+echo "/dev/mapper/base-overlay /media/base-overlay ext4 defaults,noatime,nodiratime,discard 0 0" >> /etc/fstab
 echo "/media/data/var/lib/jenkins /var/lib/jenkins none bind,noauto" >> /etc/fstab
 echo "/media/base-overlay/docker /var/lib/docker none bind,noauto" >> /etc/fstab
 echo "/media/base-overlay/containerd /var/lib/containerd none bind,noauto" >> /etc/fstab
